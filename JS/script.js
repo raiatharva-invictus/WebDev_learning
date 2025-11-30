2434ed9362b5235a866363afff1d90cea9f7983c
raiatharva-invictus <raiatharva24.in@gmail.com>
2025-12-01 01:46:17 +0530

    started javascript and did till var, datatype

---
+++ JS/script.js
@@ -0,0 +1,31 @@
+// variables(vars)
+
+// we have certain rules for naming of variables like variable name can only start with letter, _ or a $ symbol
+// we can't use reserved words
+// in javascript variables are case sensitive
+
+// // var a = 5;
+// var b = 12;
+// var c = "Hello this is Atharva";
+// // console.log(a + b)
+// // console.log(c)
+// // console.log(typeof a, typeof b, typeof c)
+
+// // use of var is not recommended as var makes a variable global so for modern js writing we will use only let and const since they are only accessible in whichever block they are defined , they can be global too
+
+// let a = 5; // globally available
+// {
+//     let b = 7; // local to this block only
+//     // console.log(a)
+//     // console.log(b)
+//     var z = 6; // this is global even though it is defined inside a block
+// }
+// console.log(a)
+// console.log(b)
+// console.log(z)
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+// JS has 7 primitive datatypes namely : null, Number, string, Symbol, undefined, Boolean, BigInt
+
