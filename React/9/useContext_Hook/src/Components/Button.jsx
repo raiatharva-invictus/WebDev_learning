93db8b5e02cecfc7fdf29b3309930c73e6c8b584
raiatharva-invictus <raiatharva24.in@gmail.com>
2025-12-18 23:07:03 +0530

    started useContext-hook

---
+++ React/9/useContext_Hook/src/Components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Component from './Component'
+
+const Button = () => {
+  return (
+    <div>
+      <button><Component/> I am a button</button>
+    </div>
+  )
+}
+
+export default Button
---
+++ React/9/useContext_Hook/src/Components/Component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Component = () => {
+  return (
+    <div>
+      This is component 1
+    </div>
+  )
+}
+
+export default Component
